80ff3b5e1d531114f535d96566b971dd7d49faa7
jayesh girase <jayesh girase@DESKTOP-PHRAUTH>
2021-10-27 15:36:02 -0700

    Commit1

---
+++ java/src/pack/classA.java
@@ -0,0 +1,21 @@
+package pack;
+
+public class classA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=115;
+		if(a%2==0)
+		{
+			System.out.println("a is even number");
+		}
+		else
+		{
+			System.out.println("a is odd number");
+		}
+		
+		
+	}
+
+}
